54165b7948a183b88f800935fff7401aea9c5967
Timothee Peroulakis <timperoulakis@gmail.com>
2022-08-11 21:02:13 +0200

    sum_digits

--- recursion.rb
+++ recursion.rb
@@ -1,3 +1,11 @@
-def rec_sum(n)
-  n.to_i == 0 ? 0 : n.to_i + rec_sum(n.to_i - 1)
+class Recursion
+  def self.rec_sum(n)
+    n.to_i == 0 ? 0 : n.to_i + rec_sum(n.to_i - 1)
+  end
+
+  def self.sum_digits(n)
+    return 0 if n == 0
+
+    n % 10 + sum_digits(n / 10)
+  end
 end
--- recursion_method.rb
+++ recursion_method.rb
@@ -1,27 +1,46 @@
 require 'minitest/autorun'
 require_relative 'recursion.rb'
 
-class RecursionTest < Minitest::Test
+class FirstProblemTest < Minitest::Test
   def test_zero
-    assert_equal 0, rec_sum(0)
+    assert_equal 0, Recursion.rec_sum(0)
   end
 
   def test_four
-    assert_equal 10, rec_sum(4)
+    assert_equal 10, Recursion.rec_sum(4)
   end
 
   def test_ten
-    assert_equal 55, rec_sum(10)
+    assert_equal 55, Recursion.rec_sum(10)
   end
 
   def test_rdn
     n = rand(100)
     expected = (n * (n + 1)) / 2
 
-    assert_equal expected, rec_sum(n)
+    assert_equal expected, Recursion.rec_sum(n)
   end
 
   def test_string
-    assert_equal 5050, rec_sum('100')
+    assert_equal 5050, Recursion.rec_sum('100')
+  end
+end
+
+class SecondProblemTest < MiniTest::Test
+  def test_zero
+    assert_equal 0, Recursion.sum_digits(0)
+  end
+
+  def test_twelve
+    assert_equal 3, Recursion.sum_digits(12)
+  end
+
+  def test_4321
+    assert_equal 10, Recursion.sum_digits(4321)
+  end
+
+  def test_random
+    n = rand(1000)
+    assert_equal n.digits.sum, Recursion.sum_digits(n)
   end
 end
