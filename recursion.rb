c5c5379878253ca2e622f944ad328f4632b72d68
Patrice Cadiot <pathibul.r@gmail.com>
2022-08-12 17:03:42 +0200

    Version respectant la signature

--- recursion.rb
+++ recursion.rb
@@ -1,18 +1,24 @@
 class Recursion
-  def self.word_split(sentence, words, result = [])
+  def self.word_split(sentence, words)
+    word_split_rec(sentence, words, result = [])
+  end
+
+
+  def self.word_split_rec(sentence, words, result = [])
     return result if words.empty?
 
     words.each do |word|
       if sentence.start_with?(word)
         result << word
 
-        word_split(sentence.delete_prefix(word), words - [word], result)
+        word_split_rec(sentence.delete_prefix(word), words - [word], result)
       end
     end
 
     result
   end
 end
+
 
 p Recursion.word_split('themanran', %w[the ran man])
 p Recursion.word_split('themanran', %w[clown ran man])
